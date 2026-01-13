b8e1515c01138f487b5fcc725f7739e07bfcd2e1
Alina Avanesyan <90269742+alinaavanesyan@users.noreply.github.com>
2026-01-13 12:39:39 +0300

    Extend plan normalization checks

---
+++ src/fetchgraph/planning/__init__.py
@@ -0,0 +1,9 @@
+"""Planning utilities for fetchgraph."""
+
+from .normalize import NormalizedPlan, PlanNormalizer, PlanNormalizerOptions
+
+__all__ = [
+    "NormalizedPlan",
+    "PlanNormalizer",
+    "PlanNormalizerOptions",
+]
